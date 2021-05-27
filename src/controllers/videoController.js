dd2d81891952aa24a31798cf028e5e0bdcda8810
aren0416 <aren0416@gmail.com>
2021-05-27 21:30:00 +0900

    edit page 연결

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trending = (req, res) => res.render("Home");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
